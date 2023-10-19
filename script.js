9107dea584fe4086de47e32e499b5c2547760e96
tirilkoder <tiril.koder@gmail.com>
2023-10-19 10:35:22 +0200

    Added if/else and design

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-// A function that gets activated when clicked on via onclick on the convert button in HTML
+    // henter element fra HTML dokumentet inne i funksjonen
+    // En funksjon som er satt opp til å bli aktivert via ein onclick på Konvert-knappen i HTML
 
 function unitConverter() {
     const myInput = document.querySelector("#my_input_id").value;
@@ -10,12 +11,39 @@ function unitConverter() {
     const displayValue = document.querySelector("#display_value_id");
     console.log(displayValue);
 
+    // Ein tom variabel som skal ta i mot resultatet som er satt opp under i if / else utrykk.
     let result;
 
-    if (fromValue === "cm" && toValue === "m") {
-        result = myInput * 10;
+    // Et if / else utrykk som sjekker om verdien som er gitt stemmer over ens med verdien vi ønsker.
+    if 
+    (fromValue === "cm" && toValue === "m")
+    {
+        result = myInput / 100;
+    }
+    else if (fromValue === "cm" && toValue === "km")
+    {
+        result = myInput / 100000;
+    }
+    else if (fromValue === "m" && toValue === "cm") 
+    {
+        result = myInput * 100;
+    } 
+    else if (fromValue === "m" && toValue ==="km")
+    {
+        result = myInput / 1000;
+    } 
+    else if (fromValue === "km" && toValue === "cm") 
+    {
+        result = myInput * 100000;
+    } 
+    else if (fromValue === "km" && toValue === "m")
+    {
+        result = myInput * 1000;
+    } 
+    else {
     }
 
-    displayValue.textContent = result;
+    // Resultat fra if / else utrykket som vises på nettsiden.
+    displayValue.textContent = result.toFixed(3);
 }
 
